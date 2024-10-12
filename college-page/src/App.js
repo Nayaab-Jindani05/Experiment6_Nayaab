c85a812179426079c9e5d4f55527006b6207aaf8
Nayaab-Jindani05 <d2022.nayaab.jindani@ves.ac.in>
2024-10-12 23:37:40 +0530

    Update App.js

--- college-page/src/App.js
+++ college-page/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  // Navigate is used for redirection
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import AdmissionForm from './pages/AdmissionForm';
@@ -29,8 +29,7 @@ const App = () => {
           <Route path="/admission" element={<AdmissionForm />} />
           <Route path="/departments" element={<Departments />} />
           <Route path="/enquiry" element={<Enquiry />} />
-          {/* Fallback route to Home */}
-          <Route path="*" element={<Navigate to="/" />} />  {/* Redirect all other routes to Home */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
